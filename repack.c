ed206b83db5466626b998390ebcbff27bf84455f
Done334 <1069734236@qq.com>
2025-11-16 02:07:50 +0800

    Fix issue

--- repack.c
+++ repack.c
@@ -1,38 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-
-FILE *out = fopen("logo_new.bin", "wb");
-
-char header[4096] = {0};
-fwrite(header, 1, 4096, out);
-
-char metaData[4096] = {0};
-int metaPos = 0;
-unsigned int currentOffset = 0;
-
-for(int i = 1; i < argc; i++) {
-FILE *f = fopen(argv[i], "rb");
-if (!f) continue;
-
-fseek(f, 0, SEEK_END);
-unsigned int size = ftell(f);
-fseek(f, 0, SEEK_SET);
-
-metaPos += sprintf(metaData + metaPos, "%s %u %u ", argv[i], currentOffset, size);
-
-char *buffer = malloc(size);
-fread(buffer, 1, size, f);
-fwrite(buffer, 1, size, out);
-fclose(f);
-free(buffer);
-
-currentOffset += size;
-}
-
-fseek(out, 0x10, SEEK_SET);
-fwrite(metaData, 1, metaPos, out);
-
-fclose(out);
-return 0;
+	
+	FILE *out = fopen("logo_new.bin", "wb");
+	
+	char header[4096] = {0};
+	fwrite(header, 1, 4096, out);
+	
+	char metadata[4096] = {0};
+	int metapos = 0;
+	unsigned int currentOffset = 0;
+	
+	for(int i = 1; i < argc; i++) {
+		FILE *fp = fopen(argv[i], "rb");
+		if (fp == NULL) continue;
+		
+		fseek(fp, 0, SEEK_END);
+		unsigned int size = ftell(fp);
+		fseek(fp, 0, SEEK_SET);
+		
+		metapos += sprintf(metadata + metapos, " %s %u %u", argv[i], currentOffset, size);
+		
+		char *buffer = malloc(size);
+		fread(buffer, 1, size, fp);
+		fwrite(buffer, 1, size, out);
+		fclose(fp);
+		free(buffer);
+		
+		currentOffset += size;
+	}
+	
+	if (metapos > 0 && metapos < sizeof(metadata) - 1) {
+		metadata[metapos] = ' ';
+		metapos++;
+	}
+	
+	fseek(out, 0x10, SEEK_SET);
+	fwrite(metadata, 1, metapos, out);
+	fclose(out);
+	
+	printf("Complete\nOutput: logo_new.bin\n");
+	
+	return 0;
 }
--- unpack.c
+++ unpack.c
@@ -1,32 +1,35 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+	
 int main(int argc, char *argv[]) {
-
-FILE *fp = fopen(argv[1], "rb");
-
-fseek(fp, 0x10, SEEK_SET);
-
-char fileName[50];
-unsigned int offset, size;
-
-while (fscanf(fp, "%s %u %u", fileName, &offset, &size) == 3) {
-printf("Extracting: %s (offset: %u, size: %u)\n", fileName, offset, size);
-
-long currentPos = ftell(fp);
-
-FILE *out = fopen(fileName, "wb");
-
-fseek(fp, offset + 4096, SEEK_SET);
-
-unsigned char *buffer = malloc(size);
-size_t read = fread(buffer, 1, size, fp);
-fwrite(buffer, 1, size, out);
-
-free(buffer);
-fclose(out);
-
-fseek(fp, currentPos, SEEK_SET);
-}
-fclose(fp);
-return 0;
+	
+	FILE *fp = fopen(argv[1], "rb");
+	
+	fseek(fp, 0x10, SEEK_SET);
+	
+	char filename[50];
+	unsigned int offset, size;
+	
+	while (fscanf(fp, "%s %u %u", filename, &offset, &size) == 3) {
+	printf("Output: %s (Offset: %u, Size: %u)\n", filename, offset, size);
+	
+	long currentpos = ftell(fp);
+	
+	FILE *out = fopen(filename, "wb");
+	
+	fseek(fp, offset + 4096, SEEK_SET);
+	
+	unsigned char *buffer = malloc(size);
+	size_t read = fread(buffer, 1, size, fp);
+	fwrite(buffer, 1, size, out);
+	
+	free(buffer);
+	fclose(out);
+	
+	fseek(fp, currentpos, SEEK_SET);
+	}
+	
+	fclose(fp);
+	
+	return 0;
 }
